13a87e7144af78812c96db21be1176d6242bed42
petesert <petesertthin@gmail.com>
2022-04-23 12:39:41 -0700

    initial setup: home url/view and models complete

---
+++ main_app/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+STATUS_CHOICES = (
+    ("complete", "complete"),
+    ("approved", "approved"),
+    ("missing info", "overdue"),
+    ("archived", "archived")
+)
+
+class Material(models.Model):
+    material_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=15)
+    received_qty = models.CharField(max_length=1000)
+    PO_qty = models.ForeignKey(User, on_delete=models.CASCADE)
+    unit_measure = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+
+class Invoice(models.Model):
+    job_id = models.IntegerField(primary_key=True)
+    invoice_id = models.IntegerField(primary_key=True)
+    employee_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.CharField(max_length=250)
+    name = models.CharField(max_length=10)
+    address = models.CharField(max_length=1000)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    contract_time = models.IntegerField()
+    
+    def __str__(self):
+        return self.invoice_id
+    
+    class Meta:
+        ordering = ['name']
+
+class Ticket(models.Model):
+    ticket_id = models.IntegerField()
+    ticketed_at = models.DateTimeField(auto_now_add=True)
+    job_id = models.IntegerField(primary_key=True)
+    material_id = models.ForeignKey(Material, on_delete=models.CASCADE)
+    signer = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.CharField(max_length=250)
+    quantity = models.IntegerField()
+    note = models.CharField(max_length=500)
+    status = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.ticket_id
+    
+    class Meta:
+        ordering = ['name']
+
+class Job(models.Model):
+    # job_id = models.IntegerField()
+    materials = models.ForeignKey(Material, on_delete=models.CASCADE)
+    employees = models.ForeignKey(User, on_delete=models.CASCADE)
+    # add validation keeping this to only 10 digits
+    number = models.CharField(max_length=250)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+    owner = models.CharField(max_length=300)
+    start_date = models.DateTimeField()
+    contract_time = models.IntegerField()
+    invoices = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    tickets = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+
+
+
+
+
+
